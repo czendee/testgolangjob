8e9df8e220a226a2499af6584bd7d75e793c1332
Don Carlos Zendejas <czende@hotmail.com>
2021-09-15 08:31:25 -0500

    Create test04.go

---
+++ test04.go
@@ -0,0 +1,75 @@
+package main
+
+import "log"
+
+// IsPalindromePermutation returns true if a srting is a palindrome permutation
+func IsPalindromePermutation(s string) bool {
+	set := make(map[rune]int)
+
+	for _, c := range s {
+		if c == ' ' {
+			continue
+		}
+
+		set[c]++
+	}
+
+	odd := false
+	for _, n := range set {
+		if n%2 != 0 {
+			if odd {
+				return false
+			}
+
+			odd = true
+		}
+	}
+
+	return true
+}
+
+// IsPalindromePermutationSimplified returns true if a srting is a palindrome permutation
+// It uses a single for loop to check the permutations
+func IsPalindromePermutationSimplified(s string) bool {
+	set := make(map[rune]int)
+	odd := 0
+
+	for _, c := range s {
+		if c == ' ' {
+			continue
+		}
+
+		set[c]++
+
+		if set[c]%2 != 0 {
+			odd++
+		} else {
+			odd--
+		}
+	}
+
+	return !(odd > 1)
+}
+
+func test01Fail(){
+    if(     IsPalindromePermutation(" wert")){
+        log.Print("IsPalindromePermutation true")
+     }else{
+         log.Print("IsPalindromePermutation false")
+     }
+
+}
+
+func test01Sucess(){
+    if(     IsPalindromePermutation(" wert")){
+        log.Print("IsPalindromePermutation true")
+     }else{
+         log.Print("IsPalindromePermutation false")
+     }
+
+}
+
+func main() {
+     test01Fail();
+     test01Sucess()
+}
