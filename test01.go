84840c0b51aa24ccde6214da7a61a9f0348c887d
Don Carlos Zendejas <czende@hotmail.com>
2021-09-14 19:11:15 -0500

    Create test01.go
    test 001

---
+++ test01.go
@@ -0,0 +1,60 @@
+package main
+
+import "log"
+
+
+// IsUnique determine if a string has all unique chars
+// This solution is based on HashTable (Go map) data structure
+func IsUnique(s string) bool {
+	set := make(map[rune]bool)
+
+	for _, c := range s {
+		if _, ok := set[c]; ok {
+			return false
+		}
+
+		set[c] = true
+	}
+
+	return true
+}
+
+// IsUniqueVanilla determine if a string has all unique chars
+// This solution doesn't utilize any additional data structures
+func IsUniqueVanilla(s string) bool {
+	lens := len(s)
+	for i := 0; i < lens; i++ {
+		for j := i + 1; j < lens; j++ {
+			if s[i] == s[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+
+
+func testIsUniqueVanillaFail(){
+    if(     IsUniqueVanilla("wert")){
+        log.Print("IsUniqueVanilla true")
+     }else{
+         log.Print("IsUniqueVanilla  false")
+     }
+
+}
+
+func testIsUniqueVanillaSucess(){
+    if(     IsUniqueVanilla("werttt")){
+        log.Print("IsUniqueVanilla true")
+     }else{
+         log.Print("IsUniqueVanilla  false")
+     }
+
+}
+
+func main() {
+     testIsUniqueVanillaFail();
+     testIsUniqueVanillaSucess()
+}
